d02e368db9710ab7b567e6ef057fc305ab597557
barrycunningham <cunnbarry@gmail.com>
2019-04-23 20:10:46 +0000

    Added all x 8 men products. Commit prior to adding Categories

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
 admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
-# Create your models here.
+## CREDIT
+## https://www.youtube.com/watch?v=Drsgp95t-sA
+## Master Code Online
+## Adding Categories to our blog
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    def __str__(self):
+        return self.name
+## END CREDIT
+
+
+# Product/image model
 class Product(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=254)
+    category = models.ForeignKey(Category, default=None)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
