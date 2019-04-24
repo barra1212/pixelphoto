2af7c18502d381c55838da7e9d65a4d700580747
barrycunningham <cunnbarry@gmail.com>
2019-04-24 10:57:03 +0000

    Filtering products by category completed

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_products
+from django.views.generic.base import TemplateView
+from .views import all_products, men, sports, women
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^men/', men, name='products_list_men'),
+    url(r'^sports/', sports, name='products_list_sports'),
+    url(r'^women/', women, name='products_list_women'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,3 +5,15 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+    
+def men(request):
+    products_list_men = Product.objects.filter(category__name='men')
+    return render(request, 'men.html', {"products_list_men": products_list_men})
+
+def sports(request):
+    products_list_sports = Product.objects.filter(category__name='sports')
+    return render(request, 'sports.html', {"products_list_sports": products_list_sports})
+
+def women(request):
+    products_list_women = Product.objects.filter(category__name='women')
+    return render(request, 'women.html', {"products_list_women": products_list_women})
